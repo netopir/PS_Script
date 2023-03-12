899646144b6dcf50206b3a846a60f4ac15dee49e
netopir <neto_pir@hotmail.com>
2023-03-12 12:59:27 +0100

    Add files via upload

---
+++ Open-File.ps1
@@ -0,0 +1,22 @@
+﻿function Open-File([string] $initialDirectory){
+
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "All files (*.*)| *.*"
+    $OpenFileDialog.ShowDialog() |  Out-Null
+
+    return $OpenFileDialog.filename
+} 
+
+$OpenFile = Open-File $env:USERPROFILE
+
+Write-Output $OpenFile
+
+Add-Type -AssemblyName System.Windows.Forms
+$Folder = New-Object System.Windows.Forms.FolderBrowserDialog
+$Folder.ShowNewFolderButton = $true
+$Folder.SelectedPath = [System.Environment]::GetFolderPath("NetworkShortcuts")
+[void]$Folder.ShowDialog()
+$Folder.SelectedPath
---
+++ Save-File.ps1
@@ -0,0 +1,15 @@
+﻿function Save-File([string] $initialDirectory){
+
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+    $OpenFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+    $OpenFileDialog.initialDirectory = $initialDirectory
+    $OpenFileDialog.filter = "All files (*.*)| *.*"
+    $OpenFileDialog.ShowDialog() |  Out-Null
+
+    return $OpenFileDialog.filename
+}
+
+$SaveFile = Save-File $env:USERPROFILE
+
+Write-Output $SaveFile
