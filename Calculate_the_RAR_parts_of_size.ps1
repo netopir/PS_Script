0aa8fedd122d5f273e4757d71f8c0439a6e47261
netopir <neto_pir@hotmail.com>
2023-07-11 19:52:23 +0200

    Add files via upload

---
+++ Calculate_the_RAR_parts_of_size.ps1
@@ -0,0 +1,14 @@
+﻿$source = "\\NASHDSERVER\Public4\SabNzb\BluRay\Imax.Sea.Rex.3D.2010.AVC.DTS.HD-MA5.1-Untouched"
+$rarvolumes = 150
+
+Write-Host "`nShow the Size of Source Folder" -ForegroundColor Green
+$FolderSize = "{0:N0}" -f ((Get-ChildItem $source -Recurse | Measure-Object -Property Length -Sum -ErrorAction Stop).Sum / 1MB)
+Write-Output $FolderSize
+
+Write-Host "`nRemove dots from the Numbers" -ForegroundColor Green
+$OnlyNumbers = $folderSize.Replace('.','')
+Write-Output $OnlyNumbers
+
+Write-Host "`nShow the Resolt of Rar" -ForegroundColor Green
+$RarFiles = ( $OnlyNumbers / $rarvolumes )
+[math]::round($RarFiles)
