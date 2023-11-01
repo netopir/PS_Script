cf1e6344907d6dba56a6f4393941a44a15338136
netopir <neto_pir@hotmail.com>
2023-11-01 21:36:48 +0100

    Update Create_SFV_check_from_folder_for_file.ps1

--- Create_SFV_check_from_folder_for_file.ps1
+++ Create_SFV_check_from_folder_for_file.ps1
@@ -2,7 +2,7 @@
 
 $archivename = 'SdLNoPDSLiAEpasUMhbLRiEETECRTDYMlTDMFEnBinSEXAADaERoEOUCaDsA'
 
-$destination = "C:\Users\Martin\Desktop\Usenet_Programms\ngPost_bluray\tmp\Mondbasis.Alpha.1.1975.S01.DiSC.6.EXTENDED.REMASTERED.DUAL.COMPLETE.BLURAY-Fiona"
+$destination = "C:\Users\Martin\Desktop\Usenet_Programms\"
 
 Set-Location $destination
 
