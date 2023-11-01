c81c13387e62a610e4e599fd2bee4af48ff18bbd
netopir <neto_pir@hotmail.com>
2023-11-01 21:35:46 +0100

    Update Count_files_in_a_folder.ps1

--- Count_files_in_a_folder.ps1
+++ Count_files_in_a_folder.ps1
@@ -1,2 +1,2 @@
 ﻿Write-Host "`nCount all files in Folder" -ForegroundColor Green
-Write-Host ( Get-ChildItem -File -Recurse 'C:\Users\Martin\Desktop\Usenet_Programms\ngPost_bluray\tmp\Bad.Boys.For.Life.2020.MULTi.COMPLETE.BLURAY-GLiMMER' | Measure-Object ).Count -NoNewline 'files'
+Write-Host ( Get-ChildItem -File -Recurse 'C:\Users\Martin\Desktop\Usenet_Programms\ngPost_bluray\tmp\' | Measure-Object ).Count -NoNewline 'files'
